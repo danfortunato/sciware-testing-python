2082e452621148d5908bff1b308c630fcd10ba00
danfortunato <dan.fortunato@gmail.com>
2021-04-29 16:40:45 -0400

    Add tests

--- sciware_testing_python/exercise.py
+++ sciware_testing_python/exercise.py
@@ -17,12 +17,14 @@ def sum_numbers(number_list):
 
     Example
     -------
-    >>> sum_numbers([1,2,3])
+    >>> a = [1,2,3]
+    >>> sum_numbers(a)
     6
 
-    Add another doctest below
-    >>> 1
-    1
+    Example
+    -------
+    >>> sum_numbers([])
+    0
     """
 
     sum_val = 0
@@ -51,7 +53,7 @@ def add_vectors(vector_1, vector_2):
     add_vec = []
 
     for a, b in zip(vector_1, vector_2):
-        add_vec.append(a * b)
+        add_vec.append(a + b)
 
     return add_vec
 
@@ -64,4 +66,10 @@ def count_ones(input_list):
     return count
 
 # Make a new function which counts the number of twos in a list
-#def count_twos(input_list):
+def count_twos(input_list):
+    count = 0
+    for n in input_list:
+        if n == 2:
+            count += 1
+
+    return count
--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 from sciware_testing_python import sum_numbers, add_vectors
 
 def test_sum_numbers_123():
@@ -8,17 +9,25 @@ def test_sum_numbers_123():
 
 def test_sum_numbers_yours():
     # write another test of the sum_numbers function
-    pass
+    sum = sum_numbers([-1.1, 0, 9, -9, 0.6, 0.5])
+    assert np.isclose(sum, 0)
 
 def test_sum_numbers_empty():
     # what's the sum of an empty list?
-    pass
+    sum = sum_numbers([])
+    assert sum == 0
 
-#@pytest.mark.xfail(strict=True, raises=TypeError)
+@pytest.mark.xfail(strict=True, raises=TypeError)
 def test_sum_strings():
-    #assert sciware_testing_python.sum_numbers(["1","2","3"]) == "123"
-    pass
+    assert sum_numbers(["1","2","3"]) == "123"
 
 # Write a test for the add_vectors function
+def test_add_vectors():
+    a = [-3, 5.5, 1]
+    b = [0, -4.5, 2]
+    c = add_vectors(a, b)
+    assert c == [-3, 1, 3]
 
 # Write a test for sum_numbers on a list of booleans
+def test_sum_bools():
+    assert sum_numbers([True, False, True]) == 2
